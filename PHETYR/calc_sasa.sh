efce2fcba81c5aead6828fdb5de13e7ab9c52e0d
David <daviddesancho@gmail.com>
2025-07-09 18:01:45 +0200

    keeping what is essential

--- PHETYR/calc_sasa.sh
+++ PHETYR/calc_sasa.sh
@@ -49,25 +49,25 @@ wat="tip3p"
 #done
 
 # SLAB REPLICATES
-wat="tip3p"
-for soup in soup_XXXL soupF_XXXL 
-do
-    for rep in 0 1 2
-    do
-        for lambda in 0 1 
-        do
-    	    tpr="../data/${proot}_${soup}_${ff}TRUE_${wat}_rep${rep}_l${lambda}_npt"
-            xtc="../data/${proot}_${soup}_${ff}TRUE_${wat}_rep${rep}_l${lambda}_npt_proc"
-            out="../analysis/${proot}_${soup}_${ff}TRUE_${wat}_rep${rep}_l${lambda}_npt_sasa"
-           gmx sasa -s $tpr -f $xtc -o $out -dt 10 -e 1000000 <<EOF &
-16
-EOF
-        done
-    wait
-    done
-done
+#wat="tip3p"
+#for soup in soupF_XXXL #soupF_XXXL  
+#do
+#for rep in 0 1 2
+#do
+#    for lambda in 0 1 
+#    do
+#        tpr="../data/${proot}_${soup}_${ff}TRUE_${wat}_rep${rep}_l${lambda}_npt"
+#        xtc="../data/${proot}_${soup}_${ff}TRUE_${wat}_rep${rep}_l${lambda}_npt_proc"
+#        out="../analysis/${proot}_${soup}_${ff}TRUE_${wat}_rep${rep}_l${lambda}_npt_sasa"
+#       gmx sasa -s $tpr -f $xtc -o $out -dt 10 -e 1000000 <<EOF &
+#16
+#EOF
+#    done
+#wait
+#done
+#done
 
-# SOUPS
+# SOUP HIGH TEMPERATURE
 #wat="tip3p"
 #for soup in soupF_XXXL soup_XXXL 
 #do
@@ -79,5 +79,17 @@ done
 #EOF
 #done
 
-rm \#* data/\#*
+# SOUP TIP4PEw
+wat="tip4pew"
+soup="soup_XXXL"
+for lambda in 0 1 
+do
+   tpr="../data/${proot}_${soup}_${ff}TRUE_${wat}_l${lambda}_npt"
+   xtc="../data/${proot}_${soup}_${ff}TRUE_${wat}_l${lambda}_npt_proc"
+   out="../analysis/${proot}_${soup}_${ff}TRUE_${wat}_l${lambda}_npt_sasa"
+   gmx sasa -s $tpr -f $xtc -o $out -dt 10 -e 1000000 <<EOF &
+16
+EOF
+done
 
+rm \#* data/\#*
