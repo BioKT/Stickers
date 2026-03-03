128632629b515070116b2d52d6bb1ec2cef74289
David <daviddesancho@gmail.com>
2026-03-03 10:33:32 +0100

    last few changes before submission

--- ARGLYS/setup_amber_pdb2gmx.sh
+++ ARGLYS/setup_amber_pdb2gmx.sh
@@ -1,31 +1,71 @@
 #!/bin/bash
 
 #export GMXLIB=/Users/daviddesancho/Research/software/pmx/pmx/data/mutff45
-#export GMXLIB=/home/david/Research/Projects/Simulation/PMX/mutff45
+export GMXLIB=/home/david/Research/Projects/Simulation/PMX/mutff45
 
 ff="amber99sb-star-ildn"
 wat="tip3p"
 host=K
-for pdb in GG${host}GG #SS${host}SS AA${host}AA YG${host}GY FG${host}GF
-do
-    out="../data/${pdb}_${ff}.pdb"
-    top="../data/${pdb}_${ff}.top"
-    itp="../data/${pdb}_${ff}_posre.itp"
-    gmx pdb2gmx -f ${pdb}.pdb -o ${out} -p $top -i $itp -ignh  <<EOF
+
+
+pdb=lys
+out="../data/${pdb}_${ff}.pdb"
+top="../data/${pdb}_${ff}.top"
+itp="../data/${pdb}_${ff}_posre.itp"
+gmx pdb2gmx -f ${pdb}.pdb -o ${out} -p $top -i $itp -ignh  <<EOF
 1
 1
 EOF
-done
+
 exit
 
-for pdb in phe tyr ser gly 
+
+
+#for pdb in GG${host}GG #SS${host}SS AA${host}AA YG${host}GY FG${host}GF
+#do
+#    out="../data/${pdb}_${ff}.pdb"
+#    top="../data/${pdb}_${ff}.top"
+#    itp="../data/${pdb}_${ff}_posre.itp"
+#    gmx pdb2gmx -f ${pdb}.pdb -o ${out} -p $top -i $itp -ignh  <<EOF
+#1
+#1
+#EOF
+#done
+#exit
+
+
+#for pdb in GG${host}GG #SS${host}SS AA${host}AA YG${host}GY FG${host}GF
+#do
+#    out="../data/${pdb}_${ff}.pdb"
+#    top="../data/${pdb}_${ff}.top"
+#    itp="../data/${pdb}_${ff}_posre.itp"
+#    gmx pdb2gmx -f ${pdb}.pdb -o ${out} -p $top -i $itp -ignh  <<EOF
+#1
+#1
+#EOF
+#done
+#exit
+
+#for pdb in arg lys #phe tyr ser gly 
+#do
+#    out="../data/${pdb}_${ff}TRUE.pdb"
+#    top="${pdb}_${ff}TRUE.top"
+#    itp="${pdb}_${ff}TRUE_posre.itp"
+#    gmx pdb2gmx -f ${pdb}.pdb -o ${out} -p $top -i $itp -ignh  <<EOF
+#1
+#1
+#EOF
+#done
+
+for pdb in GRGGY GRGGF GKGGY GKGGF
 do
-    out="../data/${pdb}_${ff}TRUE.pdb"
-    top="${pdb}_${ff}TRUE.top"
-    itp="${pdb}_${ff}TRUE_posre.itp"
+    out="../data/${pdb}_${ff}.pdb"
+    top="${pdb}_${ff}.top"
+    itp="${pdb}_${ff}_posre.itp"
     gmx pdb2gmx -f ${pdb}.pdb -o ${out} -p $top -i $itp -ignh  <<EOF
 1
 1
 EOF
 done
+
 rm \#* ../data/\#*
